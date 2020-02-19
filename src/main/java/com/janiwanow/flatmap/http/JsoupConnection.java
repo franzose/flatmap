46bda38e3cd786af6c30856800876dd5dcf8fd0f
Yan Ivanov <iwanow.jan@gmail.com>
2020-02-19 13:48:42 +0700

    wrote javadoc

--- src/main/java/com/janiwanow/flatmap/http/JsoupConnection.java
+++ src/main/java/com/janiwanow/flatmap/http/JsoupConnection.java
@@ -8,11 +8,23 @@ import java.net.SocketTimeoutException;
 import java.net.URL;
 import java.util.Optional;
 
+/**
+ * The default implementation of a connection fetching HTML documents.
+ *
+ * As stated in the class name, the current implementation relies on {@link org.jsoup.Connection}
+ * to fetch the HTML documents and has built-in support for retries in case of failures.
+ */
 public class JsoupConnection implements Connection {
     public static final class Options {
         public final int retries;
         public final int timeout;
 
+        /**
+         * Constructs connection options.
+         *
+         * @param retries number of retries before giving up
+         * @param timeout connection timeout
+         */
         public Options(int retries, int timeout) {
             this.retries = retries;
             this.timeout = timeout;
@@ -28,8 +40,12 @@ public class JsoupConnection implements Connection {
     /**
      * Fetches an HTML document from the given URL.
      *
-     * @param url a URL to fetch the document from
-     * @return
+     * Does as much retries as defined by the connection {@link Options}.
+     * Exceptions are swallowed to avoid breaking the normal program flow.
+     *
+     * @param url The URL to fetch the document from
+     * @return optional containing the fetched document or
+     *         empty optional in case of a failure
      */
     @Override
     public Optional<Document> fetch(URL url) {
