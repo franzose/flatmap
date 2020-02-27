5d7dd701b3e72b51db3054241cc97902f7dc33b3
Yan Ivanov <iwanow.jan@gmail.com>
2020-02-27 17:29:57 +0700

    added some comments

--- src/main/java/com/janiwanow/flatmap/data/ApartmentInfo.java
+++ src/main/java/com/janiwanow/flatmap/data/ApartmentInfo.java
@@ -2,12 +2,21 @@ package com.janiwanow.flatmap.data;
 
 import java.net.URL;
 
+/**
+ * Data class representing basic apartment information details.
+ */
 public final class ApartmentInfo {
     public final URL url;
     public final String address;
     public final Space space;
     public final Price price;
 
+    /**
+     * @param url URL of the original offer page from which the information was taken
+     * @param address Apartment address
+     * @param space Apartment area details
+     * @param price Rental or purchase price
+     */
     public ApartmentInfo(URL url, String address, Space space, Price price) {
         this.url = url;
         this.address = address;
--- src/main/java/com/janiwanow/flatmap/data/Space.java
+++ src/main/java/com/janiwanow/flatmap/data/Space.java
@@ -1,11 +1,20 @@
 package com.janiwanow.flatmap.data;
 
+/**
+ * Data class representing apartment area details including number of rooms.
+ */
 public final class Space {
     public final double total;
     public final double living;
     public final double kitchen;
     public final int rooms;
 
+    /**
+     * @param total Total apartment area, Sq m.
+     * @param living Living space, Sq m.
+     * @param kitchen Kitchen area, Sq m.
+     * @param rooms Number of rooms in the apartment
+     */
     public Space(double total, double living, double kitchen, int rooms) {
         this.total = total;
         this.living = living;
