bb5a6fd150f80a8e4c44142c6513eae3f9cb1c4d
Yan Ivanov <iwanow.jan@gmail.com>
2020-03-06 17:54:06 +0700

    updated EntryPoint

--- src/main/java/com/janiwanow/flatmap/EntryPoint.java
+++ src/main/java/com/janiwanow/flatmap/EntryPoint.java
@@ -5,8 +5,17 @@ import com.janiwanow.flatmap.cli.CommandNotFoundException;
 import com.janiwanow.flatmap.db.HikariConnectionFactory;
 import com.janiwanow.flatmap.db.cli.PurgeDatabaseCommand;
 import com.janiwanow.flatmap.db.cli.SetupDatabaseCommand;
+import com.janiwanow.flatmap.event.EventDispatcher;
+import com.janiwanow.flatmap.event.GreenRobotEventDispatcher;
+import com.janiwanow.flatmap.http.JsoupHttpConnection;
+import com.janiwanow.flatmap.parser.cli.ParseWebsitesCommand;
+import com.janiwanow.flatmap.parser.impl.n1.N1Parser;
+import com.janiwanow.flatmap.parser.impl.sakhcom.SakhcomParser;
+import org.greenrobot.eventbus.EventBus;
 
 import java.util.Set;
+
+import static com.janiwanow.flatmap.util.Env.ENV;
 
 /**
  * The entry point of the console application.
@@ -16,10 +25,31 @@ public final class EntryPoint {
         var dbConnectionFactory = HikariConnectionFactory.getInstance();
         var app = new Application(Set.of(
             new SetupDatabaseCommand(dbConnectionFactory),
-            new PurgeDatabaseCommand(dbConnectionFactory)
-            // more to come
+            new PurgeDatabaseCommand(dbConnectionFactory),
+            setUpParsingCommand()
         ));
 
         app.run(args);
+    }
+
+    private static ParseWebsitesCommand setUpParsingCommand() {
+        var separator = ENV.get("SEPARATOR", ";");
+
+        return new ParseWebsitesCommand(
+            setUpEventDispatcher(),
+            JsoupHttpConnection.builder(),
+            Set.of(
+                new N1Parser(Set.of(ENV.get("N1_CITIES", "novosibirsk").split(separator))),
+                new SakhcomParser(
+                    JsoupHttpConnection.builder(),
+                    Set.of(ENV.get("SAKHCOM_CITIES", "ys").split(separator))
+                )
+            )
+        );
+    }
+
+    private static EventDispatcher setUpEventDispatcher() {
+        // TODO: set up event listeners
+        return new GreenRobotEventDispatcher(EventBus.getDefault());
     }
 }
