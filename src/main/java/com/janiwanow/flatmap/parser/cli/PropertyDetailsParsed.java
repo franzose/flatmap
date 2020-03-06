34145892579fdcbc8345de3d6a7c3681924d7035
Jan Iwanow <iwanow.jan@gmail.com>
2020-03-06 20:09:02 +0700

    renamed things

--- src/main/java/com/janiwanow/flatmap/parser/cli/PropertyDetailsParsed.java
+++ src/main/java/com/janiwanow/flatmap/parser/cli/PropertyDetailsParsed.java
@@ -4,12 +4,13 @@ import com.janiwanow.flatmap.data.PropertyDetails;
 import com.janiwanow.flatmap.event.Event;
 
 /**
- * An event raised when {@link ParseWebsitesCommand} finished parsing and there were some apartments found.
+ * An event raised when {@link ParseWebsitesCommand} finished parsing
+ * and there were some property details found.
  */
 public final class PropertyDetailsParsed implements Event {
-    public final PropertyDetails[] apartments;
+    public final PropertyDetails[] items;
 
-    public PropertyDetailsParsed(PropertyDetails[] apartments) {
-        this.apartments = apartments;
+    public PropertyDetailsParsed(PropertyDetails[] items) {
+        this.items = items;
     }
 }
