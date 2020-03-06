176407ec048efd7432237fcada842428e36bf144
Jan Iwanow <iwanow.jan@gmail.com>
2020-03-06 20:17:33 +0700

    changed setup SQL

--- src/main/resources/setup_required_tables.sql
+++ src/main/resources/setup_required_tables.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS apartment (
+CREATE TABLE IF NOT EXISTS property (
     id UUID PRIMARY KEY NOT NULL,
     offer_url VARCHAR (512) UNIQUE NOT NULL,
     address VARCHAR (512) NOT NULL,
@@ -8,8 +8,8 @@ CREATE TABLE IF NOT EXISTS apartment (
     rooms SMALLINT NOT NULL CHECK (rooms > 0),
     price_amount NUMERIC (20, 2) NOT NULL CHECK (price_amount > 0.0),
     price_currency VARCHAR (4) NOT NULL,
-    last_checked_at TIMESTAMP NOT NULL,
-    invalidated_at TIMESTAMP NOT NULL
+    last_checked_at TIMESTAMP NULL,
+    invalidated_at TIMESTAMP NULL
 );
 
 CREATE TABLE IF NOT EXISTS facility_type (
@@ -23,17 +23,7 @@ CREATE TABLE IF NOT EXISTS facility (
     facility_type_id UUID NOT NULL REFERENCES facility_type ON UPDATE CASCADE ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS apartment_facility (
-    apartment_id UUID NOT NULL REFERENCES apartment ON UPDATE CASCADE ON DELETE CASCADE,
+CREATE TABLE IF NOT EXISTS property_facility (
+    property_id UUID NOT NULL REFERENCES property ON UPDATE CASCADE ON DELETE CASCADE,
     facility_id UUID NOT NULL REFERENCES facility ON UPDATE CASCADE ON DELETE CASCADE
 );
-
-INSERT INTO facility_type (id, name, description)
-VALUES
-    ('12ffa84b-99d8-4fb3-910a-b10f44756261', 'bus', ''),
-    ('da8decdc-1eb1-45e9-b03a-dd42b92dd9a9', 'underground', ''),
-    ('c11d1133-6c64-48f0-afc6-d17d0df956e1', 'tram', ''),
-    ('0a168458-2023-438c-a999-1902d3de35ab', 'supermarket', ''),
-    ('87ba75e2-7735-4d4b-9915-a246d3c25b03', 'cafe', ''),
-    ('1c3cbb9b-0704-42ee-9276-807ae8c3024b', 'restaurant', '')
-;
