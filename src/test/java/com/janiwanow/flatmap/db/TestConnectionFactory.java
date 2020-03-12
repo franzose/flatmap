625ec6939e81f615cd3c7496aa30bd32149d2480
Yan Ivanov <iwanow.jan@gmail.com>
2020-03-12 13:42:39 +0700

    fixed #15, added a check whether variables are set

--- src/test/java/com/janiwanow/flatmap/db/TestConnectionFactory.java
+++ src/test/java/com/janiwanow/flatmap/db/TestConnectionFactory.java
@@ -1,11 +1,13 @@
 package com.janiwanow.flatmap.db;
 
+import com.janiwanow.flatmap.util.Env;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Properties;
+import java.util.Set;
 
 import static com.janiwanow.flatmap.util.Env.ENV;
 
@@ -13,11 +15,18 @@ public final class TestConnectionFactory implements ConnectionFactory {
     private static final TestConnectionFactory INSTANCE;
 
     static {
+        Env.ensureVarsAreSet(Set.of(
+            "TEST_DB_PORT",
+            "TEST_DB_USER",
+            "TEST_DB_PASSWORD",
+            "TEST_DB_NAME"
+        ));
+
         Properties props = new Properties();
         props.setProperty("dataSourceClassName", "org.postgresql.ds.PGSimpleDataSource");
-        props.setProperty("dataSource.portNumber", ENV.get("TEST_DB_PORT", ENV.get("DB_PORT")));
-        props.setProperty("dataSource.user", ENV.get("TEST_DB_USER", ENV.get("DB_USER")));
-        props.setProperty("dataSource.password", ENV.get("TEST_DB_PASSWORD", ENV.get("DB_PASSWORD")));
+        props.setProperty("dataSource.portNumber", ENV.get("TEST_DB_PORT"));
+        props.setProperty("dataSource.user", ENV.get("TEST_DB_USER"));
+        props.setProperty("dataSource.password", ENV.get("TEST_DB_PASSWORD"));
         props.setProperty("dataSource.databaseName", ENV.get("TEST_DB_NAME"));
 
         INSTANCE = new TestConnectionFactory(new HikariDataSource(new HikariConfig(props)));
